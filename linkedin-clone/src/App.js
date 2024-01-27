9ab799fcab278aee271a1f6bc83c20a6683323c6
Pratik Karanjit <pratikkaranjit@gmail.com>
2024-01-27 19:25:42 +0545

    first commit

---
+++ linkedin-clone/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="app">
+     <Header></Header>
+    </div>
+  );
+}
+
+export default App;
---
+++ linkedin-clone/src/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1>This is header.</h1>
+      <div className="header_left">
+        <img src="" alt=""></img>
+      </div>
+
+      <div className="header_search"></div>
+      
+      <div className="header_right"></div>
+    </div>
+  );
+};
+
+export default Header;
