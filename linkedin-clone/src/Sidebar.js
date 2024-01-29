aa79258ff7aa37a081e787d08c616e0f300499b7
Pratik Karanjit <pratikkaranjit@gmail.com>
2024-01-29 18:31:28 +0545

    chore: CSS styling and create components

---
+++ linkedin-clone/src/Sidebar.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import './Sidebar.css'
+import { Avatar } from '@mui/material';
+import background from '../src/photos/gradient.jpg'
+
+const Sidebar = () => {
+
+    const recentItem = (topic) => (
+        <div className='sidebar_recentItem'>
+            <span className='sidebar_hash'>#</span>
+            <p>{topic}</p>
+        </div>
+    )
+
+  return (
+    <div className='sidebar'>
+        <div className='sidebar_top'>
+            <img src={background} alt='background'/>
+            <Avatar className='sidebar_avatar'/>
+            <h2>Pratik Karanjit</h2>
+            <h2>Pratik@gmail.com</h2>
+        </div>
+
+        <div className='sidebar_stats'>
+        <div className='sidebar_stat'>
+        <p>Who viewed you</p>
+        <p className='sidebar_statNumber'>2,433</p>
+        </div>
+
+        <div className='sidebar_stat'>
+        <p>Views on post</p>
+        <p className='sidebar_statNumber'>1,033</p>
+        </div>
+        </div>
+
+        <div className='sidebar_bottom'>
+            <p>Recent</p>
+            {recentItem('reactJS')}
+            {recentItem('Programming')}
+            {recentItem('Software Engineer')}
+            {recentItem('Design')}
+            {recentItem('MongoDB')}
+        </div>
+    </div>
+  )
+}
+
+export default Sidebar
